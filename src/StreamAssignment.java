7a5be6c0db4734fa2a6d8a1b047422f30a28d2f6
briantorluemke <briantorluemke@gmail.com>
2024-02-13 13:50:42 -0600

    Adding more practice

--- src/StreamAssignment.java
+++ src/StreamAssignment.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class StreamAssignment {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamAssignment {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+        List<Integer> result = numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .map(number -> number*number)
+                .collect(Collectors.toList());
+        System.out.println(result);
+    }
 }
